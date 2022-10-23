730a12e3546f11d62cca016782886fd1b098a656
hlh <hlh1783@naver.com>
2022-10-24 02:54:01 +0900

    [fix]

--- js/greetings.js
+++ js/greetings.js
@@ -4,7 +4,7 @@ const USERNAME_KEY = "username";
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  location.href = '/'
+  location.href = '/JS-clone'
 } else {
   greeting.innerText = `Hello ${savedUsername}!`;
 }
