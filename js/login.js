d8a51eaf7b571a90ee1af5e7608120e1d555cca6
hlh <hlh1783@naver.com>
2022-10-24 02:59:50 +0900

    [fix]

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ const USERNAME_KEY = "username";
     const username = loginInput.value;
     if (username) {
         localStorage.setItem(USERNAME_KEY, username);
-        return location.href = '/home.html'
+        return location.href = '/JS-clone/home.html'
     }
 
     alert("로그인 필요")
@@ -18,7 +18,7 @@ const USERNAME_KEY = "username";
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername !== null) {
-    location.href = '/home.html'
+    location.href = '/JS-clone/home.html'
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
